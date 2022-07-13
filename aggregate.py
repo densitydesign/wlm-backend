130fcd3d9d5739ca1c49af3022df3cb5620e68cc
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-07-13 19:11:30 +0200

    Update aggregate.py

--- aggregate.py
+++ aggregate.py
@@ -1,64 +1,74 @@
 import json
 from itertools import groupby
-
 from rdflib import PROV
+import sys
 
-final_data = {} # the final data
+fileName = 'data/all_monuments_places.json'
+outputName = 'data'
+if len(sys.argv) == 2:
+    fileName = sys.argv[1]
+    outputName += '-'+fileName.split("/")[-1]
+    print("Loading data from", fileName)
 
-with open('data/all_monuments_places0_999.json') as f:
-  data = json.load(f)
-  f.close()
+final_data = {}  # the final data
 
-  for m in data:
-    m["category"] = "mapped"
-    if len(m['wlm_n'])>0:
-      m["category"] = "contest"
-      if len(m["commonsPicturesWLM"])>0:
-        m["category"] = "covered"
+with open(fileName) as f:
+    data = json.load(f)
+    f.close()
 
-  data2check = list(filter(lambda d: not 'region' in d, data))
-  # print(data2check)
+    for m in data:
+        m["category"] = "mapped"
+        if len(m['wlm_n']) > 0:
+            m["category"] = "contest"
+            if len(m["commonsPicturesWLM"]) > 0:
+                m["category"] = "covered"
 
-  data = list(filter(lambda d: ('region' in d and 'province' in d and 'municipality' in d), data))
+    data2check = list(filter(lambda d: not 'region' in d, data))
+    # print(data2check)
 
-  def nest_categories(data, key="category"):
-    result = {}
-    group_sorted = sorted(data, key=lambda d: d[key])
-    for k, g in groupby(group_sorted, lambda d: d[key]):
-      nested = list(g)
-      result[k] = len(nested)
-    return result
+    data = list(filter(lambda d: (
+        'region' in d and 'province' in d and 'municipality' in d), data))
 
-  final_data["regions"] = {}
-  final_data["aggregated"] = nest_categories(data)
+    def nest_categories(data, key="category"):
+        result = {}
+        group_sorted = sorted(data, key=lambda d: d[key])
+        for k, g in groupby(group_sorted, lambda d: d[key]):
+            nested = list(g)
+            result[k] = len(nested)
+        return result
 
-  # Nest regions
-  data_sorted = sorted(data, key=lambda d: d['region'])
-  for k, g in groupby(data_sorted, lambda d: d['region']):
-    # print(k) # region
-    nested = list(g)
-    final_data["regions"][k] = {}
-    final_data["regions"][k]["aggregated"] = nest_categories(nested)
-    final_data["regions"][k]["provinces"] = {}
+    final_data["regions"] = {}
+    final_data["aggregated"] = nest_categories(data)
 
-    # Nest provinces
-    data_sorted = sorted(nested, key=lambda d: d['province'])
-    for kk, gg in groupby(data_sorted, lambda d: d['province']):
-      # print("\t", kk) # province
-      nested = list(gg)
-      final_data["regions"][k]["provinces"][kk] = {}
-      final_data["regions"][k]["provinces"][kk]["aggregated"] = nest_categories(nested)
-      final_data["regions"][k]["provinces"][kk]["municipalities"] = {}
+    # Nest regions
+    data_sorted = sorted(data, key=lambda d: d['region'])
+    for k, g in groupby(data_sorted, lambda d: d['region']):
+        # print(k) # region
+        nested = list(g)
+        final_data["regions"][k] = {}
+        final_data["regions"][k]["aggregated"] = nest_categories(nested)
+        final_data["regions"][k]["provinces"] = {}
 
-      # Nest municipalities
-      data_sorted = sorted(nested, key=lambda d: d['municipality'])
-      for kkk, ggg in groupby(data_sorted, lambda d: d['municipality']):
-        # print("\t\t", kkk)
-        nested = list(ggg)
-        final_data["regions"][k]["provinces"][kk]["municipalities"][kkk] = {}
-        final_data["regions"][k]["provinces"][kk]["municipalities"][kkk]["aggregated"] = nest_categories(nested)
+        # Nest provinces
+        data_sorted = sorted(nested, key=lambda d: d['province'])
+        for kk, gg in groupby(data_sorted, lambda d: d['province']):
+            # print("\t", kk) # province
+            nested = list(gg)
+            final_data["regions"][k]["provinces"][kk] = {}
+            final_data["regions"][k]["provinces"][kk]["aggregated"] = nest_categories(
+                nested)
+            final_data["regions"][k]["provinces"][kk]["municipalities"] = {}
 
-with open('data/aggregated/data.json', 'w', encoding='utf-8') as f:
-  json.dump(final_data, f, ensure_ascii=False, indent=4)
+            # Nest municipalities
+            data_sorted = sorted(nested, key=lambda d: d['municipality'])
+            for kkk, ggg in groupby(data_sorted, lambda d: d['municipality']):
+                # print("\t\t", kkk)
+                nested = list(ggg)
+                final_data["regions"][k]["provinces"][kk]["municipalities"][kkk] = {}
+                final_data["regions"][k]["provinces"][kk]["municipalities"][kkk]["aggregated"] = nest_categories(
+                    nested)
 
-print("Saved in data/aggregated/data.json")
+with open('data/aggregated/'+outputName+'.json', 'w', encoding='utf-8') as f:
+    json.dump(final_data, f, ensure_ascii=False, indent=4)
+
+print("Saved in data/aggregated")
