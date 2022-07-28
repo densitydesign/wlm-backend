9c6faedd0af7b9ffa3851f80b4871c2e3aed0126
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-07-28 22:32:06 +0200

    Update aggregate.py

--- aggregate.py
+++ aggregate.py
@@ -12,7 +12,7 @@ filter_area = {
     "area_type": "province",
     "area_name": "Firenze"
 }
-collect_monuments = False
+collect_monuments = True
 
 years = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
 
