44bc3c8a93c0d84c4666592a9c5bfe0cdc1f54f8
iosonosempreio <iosonosempreio@users.noreply.github.com>
2022-07-28 10:26:57 +0200

    Collect monuments function (filename)

--- aggregate.py
+++ aggregate.py
@@ -12,7 +12,7 @@ filter_area = {
     "area_type": "province",
     "area_name": "Milano"
 }
-collect_monuments = False
+collect_monuments = True
 
 years = [2012, 2013, 2014, 2015, 2016, 2017, 2018, 2019, 2020, 2021, 2022]
 
@@ -230,6 +230,8 @@ filename = 'data/aggregated/interval-' + \
     dateInterval + '.aggregation-' + aggregation_type
 if filter_area:
     filename += '.filter-' + filter_area["area_name"]
+if collect_monuments:
+    filename += '.withMonuments'
 filename += '.json'
 
 with open(filename, 'w', encoding='utf-8') as f:
